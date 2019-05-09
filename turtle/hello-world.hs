09db8aff261dd6cfc65a967a5c6d55ec6635c98b
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-05-08 21:36:31 -0500

    hello world with turtle

---
+++ turtle/hello-world.hs
@@ -0,0 +1,6 @@
+                                    -- #!/bin/bash
+{-# LANGUAGE OverloadedStrings #-}  --
+                                    --
+import Turtle                       --
+                                    --
+main = echo "Hello, world!"         -- echo Hello, world!
